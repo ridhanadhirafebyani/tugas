e62ee3900b0f87139fa77ab78ed334084dbead78
Ridha Nadhira F <ridhaikhsan@gmail.com>
2016-09-11 22:23:54 +0700

    deklarasi, inisialisasi dan sedikit validasi revisi dari coding tadi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5027/tugas/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5027/tugas/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText etNama;
     Spinner spKls;
+    RadioGroup rgStatus;
     RadioButton Lk, Pr;
     CheckBox P, PS, SBA, Padsu;
     Button bOk;
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         etNama = (EditText) findViewById(R.id.editTextNama);
         spKls = (Spinner) findViewById(R.id.spinnerKls);
+        rgStatus = (RadioGroup) findViewById(R.id.rgStatus);
         Lk = (RadioButton) findViewById(R.id.radioButtonLaki);
         Pr = (RadioButton) findViewById(R.id.radioButtonPerem);
         P = (CheckBox) findViewById(R.id.checkBoxPram);
@@ -35,27 +38,63 @@ public class MainActivity extends AppCompatActivity {
         bOk = (Button) findViewById(R.id.buttonOK);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
+//        rgStatus.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+//
+//            }
+//        });
+
         bOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                doProcess();
+//                String nama = etNama.getText().toString();
+//                String kelas=spKls.getSelectedItem().toString();
+//                String hasilrb =null;
+//
+//                if(rgStatus.getCheckedRadioButtonId()!=-1){
+//                    RadioButton rb = (RadioButton) findViewById(rgStatus.getCheckedRadioButtonId());
+//                    hasilrb = rb.getText().toString();
+//                }
+
+//                tvHasil.setText("Data Anda sudah tersimpan" + "\n\n"
+//                +"Nama : "+nama+"\n"+ "Kelas : "+kelas+"\n"+"Jenis Kelamin : "+ hasilrb+"\n");
+
+                doClick();
+
             }
         });
     }
 
-    private void doProcess() {
-        if (isValid()) {
-            String nama = etNama.getText().toString();
-            tvHasil.setText("Data Anda sudah tersimpan" + "\n\n" + "Nama : " + nama);
+    private void doClick() {
+        String nama = etNama.getText().toString();
+        String hasil = null;
+        String hasilcb = "\n Ekstrakulikuler yang dipilih : ";
+        int startlen = hasilcb.length();
 
+        if (isValid()) {
+            if (rgStatus.getCheckedRadioButtonId() != -1) {
+                RadioButton rb = (RadioButton) findViewById(rgStatus.getCheckedRadioButtonId());
+                hasil = rb.getText().toString();
+            }
+            if (hasil == null) {
+                tvHasil.setText("Jenis Kelamin belum diisi");
+            } else {
+                if (P.isChecked()) hasilcb += P.getText() + "\n";
+                if (PS.isChecked()) hasilcb += PS.getText() + "\n";
+                if (SBA.isChecked()) hasilcb += SBA.getText() + "\n";
+                if (Padsu.isChecked()) hasilcb += Padsu.getText() + "\n";
+                if (hasilcb.length() == startlen) hasilcb += "Tidak ada pilihan";
+
+                tvHasil.setText("Data Anda \n" +
+                        "Nama : " + nama + "\nKelas" + spKls.getSelectedItem().toString() + "\n\nJenis Kelamin : " + hasil + "\n" + hasilcb);
+            }
         }
     }
 
     private boolean isValid() {
-
         boolean valid = true;
-        String hasil = null;
         String nama = etNama.getText().toString();
 
         if (nama.isEmpty()) {
@@ -67,17 +106,46 @@ public class MainActivity extends AppCompatActivity {
         } else {
             etNama.setError(null);
         }
-        if (Lk.isChecked()) {
-            hasil = Lk.getText().toString();
-        } else if (Pr.isChecked()) {
-            hasil = Pr.getText().toString();
-        }
-        if (hasil == null) {
-            tvHasil.setText("Belum memilih Jenis Kelamin");
-        } else {
-            tvHasil.setText("Jenis Kelamin" + hasil);
-        }
-
         return valid;
     }
+
+
+    //    private void doProcess() {
+//        if (isValid()) {
+//        }
+//    }
+//    private boolean isValid() {
+//
+//        boolean valid = true;
+//        String nama = etNama.getText().toString();
+//        String hasilrb = null;
+//
+//        if (nama.isEmpty()) {
+//            etNama.setError("Nama belum diisi");
+//            valid = false;
+//        } else if (nama.length() < 3) {
+//            etNama.setError("Nama minimal 3 karakter");
+//            valid = false;
+//        } else{
+//            etNama.setError(null);
+//        }
+//
+//
+//
+//        if(Lk.isChecked()){
+//            hasilrb=Lk.getText().toString();
+//        }
+//        else if (Pr.isChecked()){
+//            hasilrb=Pr.getText().toString();
+//        }
+//        if(hasilrb==null){
+//            tvHasil.setText("Belum memilih Jenis Kelamin");
+//        }
+//        else{
+//            tvHasil.setText("Jenis Kelamin : "+hasilrb);
+//        }
+//
+//
+//        return valid;
+//    }
 }
