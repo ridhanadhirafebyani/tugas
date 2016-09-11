0b22cd6e805b1e2e295f598ecffef4f0d161e7ec
Ridha Nadhira F <ridhaikhsan@gmail.com>
2016-09-11 09:18:26 +0700

    validasi nama dan jenis kelamin

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5027/tugas/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5027/tugas/MainActivity.java
@@ -2,11 +2,13 @@ package id.sch.smktelkom_mlg.tugas01.xirpl5027.tugas;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     RadioButton Lk, Pr;
     CheckBox P, PS, SBA, Padsu;
     Button bOk;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +33,51 @@ public class MainActivity extends AppCompatActivity {
         SBA = (CheckBox) findViewById(R.id.checkBoxSeni);
         Padsu = (CheckBox) findViewById(R.id.checkBoxPaduan);
         bOk = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
+        bOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                doProcess();
+            }
+        });
+    }
+
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            tvHasil.setText("Data Anda sudah tersimpan" + "\n\n" + "Nama : " + nama);
+
+        }
+    }
+
+    private boolean isValid() {
+
+        boolean valid = true;
+        String hasil = null;
+        String nama = etNama.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+        if (Lk.isChecked()) {
+            hasil = Lk.getText().toString();
+        } else if (Pr.isChecked()) {
+            hasil = Pr.getText().toString();
+        }
+        if (hasil == null) {
+            tvHasil.setText("Belum memilih Jenis Kelamin");
+        } else {
+            tvHasil.setText("Jenis Kelamin" + hasil);
+        }
+
+        return valid;
     }
 }
